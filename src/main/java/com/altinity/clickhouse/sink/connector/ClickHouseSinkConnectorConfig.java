5058d2fff2fb933c1b2e21dcdf4566087734021c
Vladislav Klimenko <sunsingerus@gmail.com>
2022-03-30 17:51:00 +0300

    dev: group connector configuration variables

--- src/main/java/com/altinity/clickhouse/sink/connector/ClickHouseSinkConnectorConfig.java
+++ src/main/java/com/altinity/clickhouse/sink/connector/ClickHouseSinkConnectorConfig.java
@@ -19,23 +19,29 @@ public class ClickHouseSinkConnectorConfig {
     static final String NAME = Const.NAME;
     public static final String TOPICS = "topics";
 
-    public static final String BUFFER_COUNT = "buffer.count";
-    public static final long BUFFER_COUNT_DEFAULT = 100;
-
     private static final Logger log = LoggerFactory.getLogger(ClickHouseSinkConnectorConfig.class.getName());
 
     // ClickHouse connection
-    private static final String CLICKHOUSE_LOGIN_INFO = "ClickHouse Login Info";
-    private static final String CONNECTOR_CONFIG = "Connector Config";
+    private static final String CONFIG_GROUP_CLICKHOUSE_LOGIN_INFO = "ClickHouse Login Info";
+    private static final String CONFIG_GROUP_CONNECTOR_CONFIG = "Connector Config";
 
-    private static final String PROVIDER_CONFIG = "provider";
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ClickHouseSinkConnectorConfig.class);
 
+    /**
+     *
+     * @param config
+     */
     public static void setDefaultValues(Map<String, String> config) {
-        setFieldToDefaultValues(config, BUFFER_COUNT, BUFFER_COUNT_DEFAULT);
+        setFieldToDefaultValues(config, ClickHouseSinkConnectorConfigVariables.BUFFER_COUNT, ClickHouseSinkConnectorConfigVariables.BUFFER_COUNT_DEFAULT);
     }
 
+    /**
+     *
+     * @param config
+     * @param field
+     * @param value
+     */
     static void setFieldToDefaultValues(Map<String, String> config, String field, Long value) {
         if (!config.containsKey(field)) {
             config.put(field, "" + value);
@@ -43,6 +49,12 @@ public class ClickHouseSinkConnectorConfig {
         }
     }
 
+    /**
+     *
+     * @param config
+     * @param key
+     * @return
+     */
     static String getProperty(final Map<String, String> config, final String key) {
         if (config.containsKey(key) && !config.get(key).isEmpty()) {
             return config.get(key);
@@ -51,84 +63,94 @@ public class ClickHouseSinkConnectorConfig {
         }
     }
 
+    /**
+     *
+     * @return
+     */
     static ConfigDef newConfigDef() {
         return new ConfigDef()
-                .define(
-                        BUFFER_COUNT,
+                    // Config Group "Connector config"
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_TOPICS_TABLES_MAP,
+                        Type.STRING,
+                        "",
+                        new TopicToTableValidator(),
+                        Importance.LOW,
+                        "Map of topics to tables (optional). Format : comma-separated tuples, e.g."
+                                + " <topic-1>:<table-1>,<topic-2>:<table-2>,... ",
+                        CONFIG_GROUP_CONNECTOR_CONFIG,
+                        0,
+                        ConfigDef.Width.NONE,
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_TOPICS_TABLES_MAP)
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.BUFFER_COUNT,
                         Type.LONG,
-                        BUFFER_COUNT_DEFAULT,
+                        ClickHouseSinkConnectorConfigVariables.BUFFER_COUNT_DEFAULT,
                         ConfigDef.Range.atLeast(1),
                         Importance.LOW,
                         "BufCount",
-                        "Connector",
+                        CONFIG_GROUP_CONNECTOR_CONFIG,
                         1,
                         ConfigDef.Width.NONE,
-                        BUFFER_COUNT)
-            // ClickHouse login info
-            .define(
-                ClickHouseConfigurationVariables.CLICKHOUSE_URL,
-                Type.STRING,
-                null,
-                new ConfigDef.NonEmptyString(),
-                Importance.HIGH,
-                "ClickHouse account url",
-                CLICKHOUSE_LOGIN_INFO,
-                0,
-                ConfigDef.Width.NONE,
-                ClickHouseConfigurationVariables.CLICKHOUSE_URL)
-            .define(
-                ClickHouseConfigurationVariables.CLICKHOUSE_USER,
-                Type.STRING,
-                null,
-                new ConfigDef.NonEmptyString(),
-                Importance.HIGH,
-                "ClickHouse user name",
-                CLICKHOUSE_LOGIN_INFO,
-                1,
-                ConfigDef.Width.NONE,
-                ClickHouseConfigurationVariables.CLICKHOUSE_USER)
-            .define(
-                ClickHouseConfigurationVariables.CLICKHOUSE_PASS,
-                Type.STRING,
-                null,
-                new ConfigDef.NonEmptyString(),
-                Importance.HIGH,
-                "ClickHouse password",
-                CLICKHOUSE_LOGIN_INFO,
-                1,
-                ConfigDef.Width.NONE,
-                ClickHouseConfigurationVariables.CLICKHOUSE_PASS)
-            .define(
-                ClickHouseConfigurationVariables.CLICKHOUSE_DATABASE,
-                Type.STRING,
-                null,
-                new ConfigDef.NonEmptyString(),
-                Importance.HIGH,
-                "ClickHouse database name",
-                CLICKHOUSE_LOGIN_INFO,
-                4,
-                ConfigDef.Width.NONE,
-                ClickHouseConfigurationVariables.CLICKHOUSE_DATABASE)
-            // ToDo: Add JVM Proxy 
-            // Connector Config
-            .define(
-                ClickHouseConfigurationVariables.CLICKHOUSE_TOPICS_TABLES_MAP,
-                Type.STRING,
-                "",
-                new TopicToTableValidator(),
-                Importance.LOW,
-                "Map of topics to tables (optional). Format : comma-separated tuples, e.g."
-                    + " <topic-1>:<table-1>,<topic-2>:<table-2>,... ",
-                CONNECTOR_CONFIG,
-                0,
-                ConfigDef.Width.NONE,
-                ClickHouseConfigurationVariables.CLICKHOUSE_TOPICS_TABLES_MAP)
-            .define(
-                PROVIDER_CONFIG,
-                Type.STRING,
-                KafkaProvider.UNKNOWN.name(),
-                new KafkaProviderValidator(),
-                Importance.LOW,
-                "Whether kafka is running on Confluent code, self hosted or other managed service");
+                        ClickHouseSinkConnectorConfigVariables.BUFFER_COUNT)
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.PROVIDER_CONFIG,
+                        Type.STRING,
+                        KafkaProvider.UNKNOWN.name(),
+                        new KafkaProviderValidator(),
+                        Importance.LOW,
+                        "Whether kafka is running on Confluent code, self hosted or other managed service",
+                        CONFIG_GROUP_CONNECTOR_CONFIG,
+                        2,
+                        ConfigDef.Width.NONE,
+                        ClickHouseSinkConnectorConfigVariables.PROVIDER_CONFIG)
+
+                    // Config Group "ClickHouse login info"
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_URL,
+                        Type.STRING,
+                        null,
+                        new ConfigDef.NonEmptyString(),
+                        Importance.HIGH,
+                        "ClickHouse account url",
+                        CONFIG_GROUP_CLICKHOUSE_LOGIN_INFO,
+                        0,
+                        ConfigDef.Width.NONE,
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_URL)
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_USER,
+                        Type.STRING,
+                        null,
+                        new ConfigDef.NonEmptyString(),
+                        Importance.HIGH,
+                        "ClickHouse user name",
+                        CONFIG_GROUP_CLICKHOUSE_LOGIN_INFO,
+                        1,
+                        ConfigDef.Width.NONE,
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_USER)
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_PASS,
+                        Type.STRING,
+                        null,
+                        new ConfigDef.NonEmptyString(),
+                        Importance.HIGH,
+                        "ClickHouse password",
+                        CONFIG_GROUP_CLICKHOUSE_LOGIN_INFO,
+                        2,
+                        ConfigDef.Width.NONE,
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_PASS)
+                    .define(
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_DATABASE,
+                        Type.STRING,
+                        null,
+                        new ConfigDef.NonEmptyString(),
+                        Importance.HIGH,
+                        "ClickHouse database name",
+                        CONFIG_GROUP_CLICKHOUSE_LOGIN_INFO,
+                        3,
+                        ConfigDef.Width.NONE,
+                        ClickHouseSinkConnectorConfigVariables.CLICKHOUSE_DATABASE)
+                        // ToDo: Add JVM Proxy
+                ;
       }
 }
--- src/main/java/com/altinity/clickhouse/sink/connector/ClickHouseSinkConnectorConfigVariables.java
+++ src/main/java/com/altinity/clickhouse/sink/connector/ClickHouseSinkConnectorConfigVariables.java
@@ -1,14 +1,17 @@
 
 package com.altinity.clickhouse.sink.connector;
 
-public class ClickHouseConfigurationVariables {
+public class ClickHouseSinkConnectorConfigVariables {
+
+    static final String BUFFER_COUNT = "buffer.count";
+
+    static final long BUFFER_COUNT_DEFAULT = 100;
+    static final String CLICKHOUSE_TOPICS_TABLES_MAP = "clickhouse.topic2table.map";
 
     static final String CLICKHOUSE_URL = "clickhouse.server.url";
     static final String CLICKHOUSE_USER = "clickhouse.server.user";
     static final String CLICKHOUSE_PASS = "clickhouse.server.pass";
     static final String CLICKHOUSE_DATABASE = "clickhouse.server.database";
 
-    static final String CLICKHOUSE_TOPICS_TABLES_MAP = "clickhouse.topic2table.map";
-
-
+    static final String PROVIDER_CONFIG = "provider";
 }
