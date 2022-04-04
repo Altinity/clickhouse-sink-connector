561aec61a26698d3df583f287679aaf0a0db8f21
Vladislav Klimenko <sunsingerus@gmail.com>
2022-04-04 11:33:07 +0300

    env: docker imagee buildedr

---
+++ docker/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Production docker image builder
+set -e
+
+# Source configuration
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+SRC_ROOT="$(realpath "${CUR_DIR}/..")"
+
+# Externally configurable build-dependent options
+TAG="${TAG:-latest}"
+DOCKER_IMAGE="altinity/clickhouse-kafka-sink-connector:${TAG}"
+
+# Externally configurable build-dependent options
+DOCKERFILE_DIR="${SRC_ROOT}/docker"
+DOCKERFILE="${DOCKERFILE_DIR}/Dockerfile"
+
+DOCKER_CMD="docker build -t ${DOCKER_IMAGE} -f ${DOCKERFILE} ${SRC_ROOT}"
+
+if ${DOCKER_CMD}; then
+    echo "ALL DONE"
+else
+    echo "FAILED"
+    exit 1
+fi
