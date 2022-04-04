fcdf02518a5031402ef685fb19deb4abe6e30a73
Vladislav Klimenko <sunsingerus@gmail.com>
2022-04-04 11:31:38 +0300

    env: move sql files

---
+++ deploy/sql/mysql_schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS test;
+USE test;
+CREATE TABLE IF NOT EXISTS employees (
+    emp_no      INT             NOT NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)
+);
