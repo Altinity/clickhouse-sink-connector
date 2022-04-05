3c808ca3dcf2ade7e7a6e1e46d60496a01216107
Vladislav Klimenko <sunsingerus@gmail.com>
2022-04-05 12:27:11 +0300

    dev: dedup

---
+++ src/main/java/com/altinity/clickhouse/sink/connector/deduplicator/DeDuplicator.java
@@ -0,0 +1,16 @@
+package com.altinity.clickhouse.sink.connector.deduplicator;
+
+
+import com.altinity.clickhouse.sink.connector.metadata.SchemaPair;
+import com.altinity.clickhouse.sink.connector.metadata.SinkConfig;
+import com.altinity.clickhouse.sink.connector.metadata.SinkRecordField;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.errors.ConnectException;
+
+import java.util.*;
+
+public class DeDuplicator {
+    public DeDuplicator() {
+    }
+}
