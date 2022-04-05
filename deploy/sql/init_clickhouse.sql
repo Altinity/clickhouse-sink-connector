9cee2c482f3696120b243f1c11dee1474f89f3af
Kanthi Subramanian <subkanthi@gmail.com>
2022-04-05 11:03:49 -0400

    Added support for remote debugging Kafka connector and entrypoint db to load data to msysql and clickhouse.

---
+++ deploy/sql/init_clickhouse.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employees
+(
+    `emp_no` Int8,
+    `birth_date` Date32,
+    `first_name` String,
+    `last_name` String,
+    `gender` String,
+    `hire_date` Date32
+)
+ENGINE = MergeTree
+PRIMARY KEY emp_no
+ORDER BY emp_no
