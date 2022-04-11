9cd3bb9b339e7e58e9aa7405a960142323e30a9d
Kanthi Subramanian <subkanthi@gmail.com>
2022-04-11 08:45:13 -0400

    Updated clickhouse schema.

--- deploy/sql/test/clickhouse.sql
+++ deploy/sql/test/clickhouse.sql
@@ -3,3 +3,7 @@ use test;
 alter table employees add column salary bigint;
 
 alter table employees add column num_years tinyint;
+
+alter table employees add column last_modified_date_time DateTime;
+
+alter table employees add column last_access_time String;
