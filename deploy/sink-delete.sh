a4efa01a760f3cf1486b02653f6a346aa2793a10
Vladislav Klimenko <sunsingerus@gmail.com>
2022-04-11 12:54:46 +0300

    env: sink setup

--- deploy/sink-delete.sh
+++ deploy/sink-delete.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# Source configuration
+CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+source "${CUR_DIR}/sink-connector-config.sh"
+
 echo "Deleting Sink Connector"
-curl -X DELETE -H "Accept:application/json" localhost:18083/connectors/sink-connector 2>/dev/null | jq .
+curl -X DELETE -H "Accept:application/json" "${CONNECTOR_URL}" 2>/dev/null | jq .
