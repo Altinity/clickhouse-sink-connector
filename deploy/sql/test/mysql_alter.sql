a3f430cfe12443e5780f9693797e6eff384e4283
Kanthi Subramanian <subkanthi@gmail.com>
2022-04-14 14:15:06 -0400

    Added support for timestamp data type, updated SQL scripts.

--- deploy/sql/test/mysql_alter.sql
+++ deploy/sql/test/mysql_alter.sql
@@ -18,14 +18,14 @@ add column last_access_time TIME;
 insert into employees values(1333446, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
  65535, 4294967295, 9223372036854775807,  127, 8388607, 32767, 2147483647, '2022-01-01 00:01:00', '01:02:03')
 
-alter table employees add char(1) married_status;
+alter table employees add column married_status char(1);
 
 insert into employees values(1333488, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
  65535, 4294967295, 9223372036854775807,  127, 8388607, 32767, 2147483647, '2022-01-01 00:01:00', '01:02:03', 'M')
 
-alter table employees add decimal(30, 12) perDiemRate;
+alter table employees add column perDiemRate decimal(30, 12);
 
-insert into employees values(1333488, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
+insert into employees values(1333489, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
  65535, 4294967295, 9223372036854775807,  127, 8388607, 32767, 2147483647, '2022-01-01 00:01:00', '01:02:03', 'M', 30.2323232)
 
 alter table employees add column hourlyRate double;
@@ -33,9 +33,14 @@ alter table employees add column hourlyRate double;
 insert into employees values(1333489, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
   65535, 4294967295, 9223372036854775807,  127, 8388607, 32767, 2147483647, '2022-01-01 00:01:00', '01:02:03', 'M', 30.2323232, 120.0123)
 
-alter table employees add text jobDescription text;
+alter table employees add column jobDescription text;
 
-insert into employees values(1333489, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
+insert into employees values(1333492, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
   65535, 4294967295, 9223372036854775807,  127, 8388607, 32767, 2147483647, '2022-01-01 00:01:00', '01:02:03', 'M', 30.2323232, 120.0123, 'Intermediate Software Engineer')
 
 
+ alter table employees add column updated_time timestamp;
+
+ insert into employees values(1333492, '1980-01-01', 'John', 'Doe', 'M', '2022-01-01', 18446744073709551615 , 255, 16777215,
+   65535, 4294967295, 9223372036854775807,  127, 8388607, 32767, 2147483647, '2022-01-01 00:01:00', '01:02:03', 'M', 30.2323232, 120.0123, 'Intermediate Software Engineer',
+   '2008-01-01 00:00:01');
