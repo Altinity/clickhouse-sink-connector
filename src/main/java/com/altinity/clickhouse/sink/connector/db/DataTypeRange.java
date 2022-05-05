dce1cb26467e3bfa1d166f92a536ef10da792ccf
Kanthi Subramanian <subkanthi@gmail.com>
2022-05-05 18:01:20 -0400

    Added logic to check if the column data type is DateTime64 for limiting the range.

--- src/main/java/com/altinity/clickhouse/sink/connector/db/DataTypeRange.java
+++ src/main/java/com/altinity/clickhouse/sink/connector/db/DataTypeRange.java
@@ -11,4 +11,7 @@ public class DataTypeRange
     public static final String CLICKHOUSE_MIN_SUPPORTED_DATETIME = "1970-01-01T00:00:00";
     public static final String CLICKHOUSE_MAX_SUPPORTED_DATETIME = "2106-02-07T06:28:15";
 
+    // DateTime
+    public static final String CLICKHOUSE_MIN_SUPPORTED_DATETIME64 = "1925-01-01T00:00:00";
+    public static final String CLICKHOUSE_MAX_SUPPORTED_DATETIME64 = "2283-11-11T23:59:59";
 }
