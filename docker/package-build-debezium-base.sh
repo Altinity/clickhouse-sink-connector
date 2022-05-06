add2973dfbb9160f94e53640ed85d9cff943fb5d
Vladislav Klimenko <sunsingerus@gmail.com>
2022-05-06 20:33:43 +0300

    build images from sources

--- docker/package-build-debezium-base.sh
+++ docker/package-build-debezium-base.sh
@@ -7,4 +7,4 @@ set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SRC_ROOT="$(realpath "${CUR_DIR}/..")"
 
-{ cd "${SRC_ROOT}"; mvn clean package; } && "${CUR_DIR}"/build.sh
+{ cd "${SRC_ROOT}"; mvn clean package; } && "${CUR_DIR}"/build-debezium-base.sh
