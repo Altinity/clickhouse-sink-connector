deef16be67f2645e88678ca08a96be256c186ff0
Vladislav Klimenko <sunsingerus@gmail.com>
2022-05-11 14:55:52 +0300

    use proper files

--- docker/build-sink-on-graalvm.sh
+++ docker/build-sink-on-graalvm.sh
@@ -9,7 +9,7 @@ SRC_ROOT="$(realpath "${CUR_DIR}/..")"
 
 # Externally configurable build-dependent options
 TAG="${TAG:-latest}"
-DOCKER_IMAGE="altinity/clickhouse-kafka-sink-connector-debezium-graalvm:${TAG}"
+DOCKER_IMAGE="altinity/clickhouse-kafka-sink-connector-graalvm:${TAG}"
 
 # Externally configurable build-dependent options
 DOCKERFILE_DIR="${SRC_ROOT}/docker"
--- docker/package-build-sink-on-debezium-base.sh
+++ docker/package-build-sink-on-debezium-base.sh
@@ -7,4 +7,4 @@ set -e
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 SRC_ROOT="$(realpath "${CUR_DIR}/..")"
 
-{ cd "${SRC_ROOT}"; mvn clean package; } && "${CUR_DIR}"/build-debezium-base.sh
+{ cd "${SRC_ROOT}"; mvn clean package; } && "${CUR_DIR}"/build-sink-on-debezium-base.sh
