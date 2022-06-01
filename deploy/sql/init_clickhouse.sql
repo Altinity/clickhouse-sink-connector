d174c68f3ecc222e97e628b390102c936163415f
Kanthi Subramanian <subkanthi@gmail.com>
2022-06-01 12:52:52 -0400

    Set flag in debezium connector to fix public key retrieval error.

--- deploy/sql/init_clickhouse.sql
+++ deploy/sql/init_clickhouse.sql
@@ -21,9 +21,9 @@ CREATE TABLE employees
     `_binlog_pos` Nullable(Int32),
     `_binlog_row` Nullable(Int32),
     `_server_thread` Nullable(Int32),
-    `sign` Int8
+    `ver` UInt32
 )
-ENGINE = CollapsingMergeTree(sign)
+ENGINE = ReplacingMergeTree(ver)
 PRIMARY KEY emp_no
 ORDER BY emp_no;
 
