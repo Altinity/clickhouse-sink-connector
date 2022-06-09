8e80109010a725ff13312545b1dfec7c1aaf0807
Kanthi Subramanian <subkanthi@gmail.com>
2022-06-09 13:20:38 -0400

    Updated mysql configuration to set max_connections to 5000. Added sysbench script to run load test of inserts.

--- deploy/run_sysbench_load_test.sh
+++ deploy/run_sysbench_load_test.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+
+#https://severalnines.com/database-blog/using-sysbench-generate-test-data-sharded-table-mysql
+#
+# --rate	Average transactions rate. The number specifies how many events (transactions) per seconds should be executed by all threads on average. 0 (default) means unlimited rate, i.e. events are executed as fast as possible
+
+sysbench \
+/usr/share/sysbench/oltp_insert.lua \
+--report-interval=2 \
+--threads=500 \
+--rate=0 \
+--time=0 \
+--db-driver=mysql \
+--mysql-host=127.0.0.1 \
+--mysql-port=3306 \
+--mysql-user=root \
+--mysql-db=sbtest \
+--mysql-password=root \
+--tables=1 \
+--table-size=1000000 \
+run
