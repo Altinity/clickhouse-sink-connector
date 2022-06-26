ce3b6c0f085dfc7eade431a151f1bbcfb40da9ee
Kanthi Subramanian <subkanthi@gmail.com>
2022-06-26 13:51:26 -0400

    Added logic to create table in CH from record

--- src/main/java/com/altinity/clickhouse/sink/connector/converters/ClickHouseAutoCreateTable.java
+++ src/main/java/com/altinity/clickhouse/sink/connector/converters/ClickHouseAutoCreateTable.java
@@ -1,5 +1,7 @@
 package com.altinity.clickhouse.sink.connector.converters;
 
+import java.util.Map;
+
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
 
@@ -8,6 +10,10 @@ public class ClickHouseAutoCreateTable {
 
     public void createClickHouseTableSyntax(Field[] fields) {
 
+        ClickHouseDataTypeMapper mapper = new ClickHouseDataTypeMapper();
+        
+        Map<String, String> columnToDataTypesMap = new HashMap<String, String>();
+        
         for(Field f: fields) {
             String colName = f.name();
 
@@ -15,6 +21,37 @@ public class ClickHouseAutoCreateTable {
             String schemaName = f.schema().name();
 
             // Input: 
+            ClickHouseDataType dataType = mapper.getClickHouseDataType(type, schemaName);
+            if(dataType != null) {
+
+            }
         }
+    }
+
+    /**
+     * Function to generate CREATE TABLE for ClickHouse.
+     * @param primaryKey
+     * @param columnToDataTypesMap
+     * @return
+     */
+    public String createTableSyntax(String primaryKey, Map<String, String> columnToDataTypesMap) {
+
+
+        StringBuilder createTableSyntax = new StringBuilder();
+
+        createTableSyntax.append("CREATE TABLE(");
+
+        for(Map.Entry<String, String>  entry: columnToDataTypesMap.entrySet()) {
+            createTableSyntax.append("`").append(entry.getKey()).append("`").append(" ").append(entry.getValue()).append(",");
+
+        }
+
+        createTableSyntax.append(")");
+
+        createTableSyntax.append(" ENGINE = MergeTree");
+        createTableSyntax.append("PRIMARY KEY ").append(primaryKey);
+        createTableSyntax.append("ORDER BY ").append(primaryKey);
+
+        return createTableSyntax.toString();
     }
 }
