6ede0af5bac48a5b6291a2b309ef17adb4644429
Kanthi Subramanian <subkanthi@gmail.com>
2022-08-18 02:10:51 -0400

    Scripts to load ontime airline data to MySQL and ClickHouse.

--- deploy/sql/load_airline_ontime_data_mysql.sh
+++ deploy/sql/load_airline_ontime_data_mysql.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 ## Download all the zip files from 1987 - 2002
-#wget --no-check-certificate --continue https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_{1987..2022}_{1..12}.zip
+wget --no-check-certificate --continue https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_{1987..2022}_{1..12}.zip
 
 ### Unzip the zip files to extract CSV files
-#unzip *.zip CSV/
+unzip *.zip CSV/
 
-#cd CSV
+cd CSV
 
 set +x
 #remove='On_Time_Reporting_Carrier_On_Time_Performance_(1987_present)'
 
-#for i in  "$remove"*;do mv "$i" "${i#"$remove"}";done
+for i in  "$remove"*;do mv "$i" "${i#"$remove"}";done
 
 for filename in *.csv
 do
