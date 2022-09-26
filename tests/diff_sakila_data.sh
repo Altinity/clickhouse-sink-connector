9bf36ce836f4d6ead3d0ab6c5a8528a8a28bdd96
Kanthi Subramanian <subkanthi@gmail.com>
2022-09-26 13:26:24 -0500

    Removed init scripts for MySQL and ClickHouse. Added logic to download sakila employees data and script to perform checksum comparison.

--- tests/diff_sakila_data.sh
+++ tests/diff_sakila_data.sh
@@ -1,11 +1,15 @@
 cd ../python
-#python db_compare/mysql_table_checksum.py --mysql_host localhost --mysql_user root --mysql_password root  --mysql_database sakila --tables_regex "^*" --debug_output
-#python db_compare/clickhouse_table_checksum.py --clickhouse_host localhost --clickhouse_user root --clickhouse_password root  --clickhouse_database sakila --tables_regex "^*" --debug_output
 
-diff out.customer.mysql.txt out.customer.ch.txt
-diff out.payment.mysql.txt out.payment.ch.txt
-diff out.rental.mysql.txt out.rental.ch.txt
+python db_compare/mysql_table_checksum.py --mysql_host localhost --mysql_user root --mysql_password root  --mysql_database sakila --tables_regex "^*" --debug_output
+python db_compare/clickhouse_table_checksum.py --clickhouse_host localhost --clickhouse_user root --clickhouse_password root  --clickhouse_database sakila --tables_regex "^*" --debug_output
+
+
+for tableName in actor address category city country customer film film_actor film_category film_text inventory language payment rental staff store
+do
+  echo " DIFF TABLE ****${tableName} **** "
+  diff out.${tableName}.mysql.txt out.${tableName}.ch.txt
+done
 #diff ../python/
 
-rm -fr *.ch.txt
-rm -fr *.mysql.txt
+#rm -fr *.ch.txt
+#rm -fr *.mysql.txt
