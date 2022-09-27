740b417609254e9a1fec02677de3996830697a31
Kanthi Subramanian <subkanthi@gmail.com>
2022-09-27 11:25:35 -0500

    Updated sysbench scripts to iterate through multiple tests and perform a diff.

--- deploy/configure_sysbench.sh
+++ deploy/configure_sysbench.sh
@@ -1,4 +1,13 @@
 DATABASE=sbtest
 ./debezium-delete.sh &&  ./debezium-connector-setup-database.sh $DATABASE && ./sink-delete.sh && ./sink-connector-setup-database.sh $DATABASE
 ./debezium-delete.sh &&  ./debezium-connector-setup-database.sh $DATABASE && ./sink-delete.sh && ./sink-connector-setup-database.sh $DATABASE
+
+docker exec -it mysql-master mysql -uroot -proot -e "CREATE SCHEMA sbtest;"
+docker exec -it mysql-master mysql -uroot -proot -e "CREATE USER 'sbtest'@'%' IDENTIFIED BY 'passw0rd';"
+docker exec -it mysql-master mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON sbtest.* TO 'sbtest'@'%';"
+
+
 docker exec -it clickhouse clickhouse-client -uroot --password root -mn --query "drop database if exists $DATABASE;create database $DATABASE;"
+
+
+
