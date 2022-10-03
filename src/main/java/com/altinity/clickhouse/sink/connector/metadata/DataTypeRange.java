1d3cdb8de1af33020377d8334f62217c4b2199d5
Kanthi Subramanian <subkanthi@gmail.com>
2022-10-03 13:45:07 -0400

    Changed date limit logic to use epoch in Days. Updated checksum to use CH date limits.

--- src/main/java/com/altinity/clickhouse/sink/connector/metadata/DataTypeRange.java
+++ src/main/java/com/altinity/clickhouse/sink/connector/metadata/DataTypeRange.java
@@ -5,15 +5,14 @@ import com.clickhouse.client.data.BinaryStreamUtils;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.sql.Date;
 
 public class DataTypeRange
 {
 
     // Set clickhouse-jdbc limits
-    public static final Date CLICKHOUSE_MIN_SUPPORTED_DATE32 = new Date(BinaryStreamUtils.DATE32_MIN);
+    public static final Integer CLICKHOUSE_MIN_SUPPORTED_DATE32 = BinaryStreamUtils.DATE32_MIN;
 
-    public static final Date CLICKHOUSE_MAX_SUPPORTED_DATE32 = new Date(BinaryStreamUtils.DATE32_MAX);
+    public static final Integer CLICKHOUSE_MAX_SUPPORTED_DATE32 = BinaryStreamUtils.DATE32_MAX;
 
 
     // DateTime
