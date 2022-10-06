a545a3cca1f66e2fcc9bd69855d0d0a891d72335
Kanthi Subramanian <subkanthi@gmail.com>
2022-10-06 15:58:50 -0400

    Changed logic to use setTimestamp in JDBC for writing DateTime fields with microseconds.

--- src/test/java/com/clickhouse/sink/connector/converters/DebeziumConverterTest.java
+++ src/test/java/com/clickhouse/sink/connector/converters/DebeziumConverterTest.java
@@ -22,35 +22,47 @@ public class DebeziumConverterTest {
         //Assert.assertTrue(formattedTime.equalsIgnoreCase("20:02:03"));
     }
 
-//    @Test
-//    public void testTimestampConverter() {
+    @Test
+    public void testMicroTimestampConverter() {
+
+
+        // With microseconds
+//        String resultWMicroSeconds = DebeziumConverter.MicroTimestampConverter.convert(1665076675000000L);
+//       // Assert.assertTrue(resultWMicroSeconds == 1665076675000L);
 //
-//        Object timestampEpoch = 1640995260000L;
-//        String formattedTimestamp = DebeziumConverter.TimestampConverter.convert(timestampEpoch, false);
-//
-//        //Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("2021-12-31 19:01:00"));
-//    }
-//
-//    @Test
-//    public void testTimestampConverterMinRange() {
-//
-//        Object timestampEpoch = -2166681362000L;
-//        String formattedTimestamp = DebeziumConverter.TimestampConverter.convert(timestampEpoch, false);
-//
-//        Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("1925-01-01 00:00:00"));
-//    }
-//
-//    @Test
-//    public void testTimestampConverterMaxRange() {
-//
-//        Object timestampEpoch = 4807440238000L;
-//        String formattedTimestamp = DebeziumConverter.TimestampConverter.convert(timestampEpoch, false);
-//
-//        System.out.println("DateTimeRagnge MIN " + DataTypeRange.CLICKHOUSE_MIN_SUPPORTED_DATETIME.toString());
-//
-//        System.out.println("DATETIME 64" + BinaryStreamUtils.DATETIME64_MIN);
-//        Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("2122-05-05 16:03:58"));
-//    }
+//        // With milliseconds
+//        String resultWMilliSeconds = DebeziumConverter.MicroTimestampConverter.convert(1665076675000L);
+        //Assert.assertTrue(resultWMilliSeconds == 1665076675L);
+
+
+    }
+
+    @Test
+    public void testTimestampConverter() {
+
+        Object timestampEpoch = 1640995260000L;
+        String formattedTimestamp = String.valueOf(DebeziumConverter.TimestampConverter.convert(timestampEpoch, false));
+
+        Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("2021-12-31 19:01:00"));
+    }
+
+    @Test
+    public void testTimestampConverterMinRange() {
+
+        Object timestampEpoch = -2166681362000L;
+        String formattedTimestamp = String.valueOf(DebeziumConverter.TimestampConverter.convert(timestampEpoch, false));
+
+        Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("1925-01-01 00:00:00"));
+    }
+
+    @Test
+    public void testTimestampConverterMaxRange() {
+
+        Object timestampEpoch = 4807440238000L;
+        String formattedTimestamp = String.valueOf(DebeziumConverter.TimestampConverter.convert(timestampEpoch, false));
+
+        Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("2122-05-05 16:03:58"));
+    }
 
     @Test
     public void testDateConverter() {
@@ -95,21 +107,19 @@ public class DebeziumConverterTest {
     public void testZonedTimestampConverter() {
 
         String formattedTimestamp = DebeziumConverter.ZonedTimestampConverter.convert("2021-12-31T19:01:00Z");
-
         Assert.assertTrue(formattedTimestamp.equalsIgnoreCase("2021-12-31 19:01:00.000000"));
 
         String formattedTimestampWMicroSeconds = DebeziumConverter.ZonedTimestampConverter.convert("2038-01-19T03:14:07.999999Z");
         Assert.assertTrue(formattedTimestampWMicroSeconds.equalsIgnoreCase("2038-01-19 03:14:07.999999"));
 
         String formattedTimestamp3 = DebeziumConverter.ZonedTimestampConverter.convert("2038-01-19T03:14:07.99Z");
-
         Assert.assertTrue(formattedTimestamp3.equalsIgnoreCase("2038-01-19 03:14:07.990000"));
+
     }
-//
-//    @Test
-//    public void testMicroTimestampConverter() {
-//
-//        String convertedString = DebeziumConverter.MicroTimestampConverter.convert(-248313600000000L);
-//        Assert.assertTrue(convertedString.equalsIgnoreCase("1962-02-18 00:00:00"));
-//    }
+
+    @Test
+    public void testCheckIfDateTimeExceedsSupportedRange() {
+        DebeziumConverter.TimestampConverter.convert(1665076675000L, false);
+    }
+
 }
