fe0b61545fec2003386cf823020dd80256357365
Kanthi Subramanian <subkanthi@gmail.com>
2023-05-02 17:17:27 -0400

    Added test to check replacingmergetree support for versions older than 23.2

--- sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/ClickHouseDebeziumEmbeddedDDLTableOperationsIT.java
+++ sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/ClickHouseDebeziumEmbeddedDDLTableOperationsIT.java
@@ -1,18 +1,25 @@
 package com.altinity.clickhouse.debezium.embedded.ddl.parser;
 
 import com.altinity.clickhouse.debezium.embedded.cdc.DebeziumChangeEventCapture;
+import com.altinity.clickhouse.debezium.embedded.config.EnvironmentConfigurationService;
 import com.altinity.clickhouse.debezium.embedded.parser.SourceRecordParserService;
 import com.altinity.clickhouse.sink.connector.db.BaseDbWriter;
 import org.apache.log4j.BasicConfigurator;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.testcontainers.containers.ClickHouseContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
@@ -21,7 +28,14 @@ import java.util.concurrent.atomic.AtomicReference;
 
 
 @Testcontainers
-public class ClickHouseDebeziumEmbeddedDDLTableOperationsIT extends ClickHouseDebeziumEmbeddedDDLBaseIT  {
+public class ClickHouseDebeziumEmbeddedDDLTableOperationsIT {
+    protected MySQLContainer mySqlContainer;
+    protected ClickHouseContainer clickHouseContainer;
+
+//    @Container
+//    public static ClickHouseContainer clickHouseContainer = new ClickHouseContainer("clickhouse/clickhouse-server:latest")
+//            .withInitScript("init_clickhouse.sql")
+//            .withExposedPorts(8123);
 
         @BeforeEach
         public void startContainers() throws InterruptedException {
@@ -34,12 +48,24 @@ public class ClickHouseDebeziumEmbeddedDDLTableOperationsIT extends ClickHouseDe
 
             BasicConfigurator.configure();
             mySqlContainer.start();
-            clickHouseContainer.start();
+           // clickHouseContainer.start();
             Thread.sleep(15000);
         }
 
-        @Test
-        public void testTableOperations() throws Exception {
+        @ParameterizedTest
+        @CsvSource({
+            "clickhouse/clickhouse-server:latest",
+            "clickhouse/clickhouse-server:22.3"
+        })
+        public void testTableOperations(String clickHouseServerVersion) throws Exception {
+                //,String rcxExpectedResult) throws Exception {
+
+            clickHouseContainer = new ClickHouseContainer(clickHouseServerVersion)
+            .withInitScript("init_clickhouse.sql")
+            .withExposedPorts(8123);
+
+            clickHouseContainer.start();
+
             AtomicReference<DebeziumChangeEventCapture> engine = new AtomicReference<>();
 
             ExecutorService executorService = Executors.newFixedThreadPool(1);
@@ -103,38 +129,71 @@ public class ClickHouseDebeziumEmbeddedDDLTableOperationsIT extends ClickHouseDe
             // Validate table created with partitions.
             String membersResult = writer.executeQuery("show create table members");
 
-            Assert.assertTrue(membersResult.equalsIgnoreCase("CREATE TABLE employees.members\n" +
-                    "(\n" +
-                    "    `firstname` String,\n" +
-                    "    `lastname` String,\n" +
-                    "    `username` String,\n" +
-                    "    `email` Nullable(String),\n" +
-                    "    `joined` Date32,\n" +
-                    "    `_version` UInt64,\n" +
-                    "    `is_deleted` UInt8\n" +
-                    ")\n" +
-                    "ENGINE = ReplacingMergeTree(_version, is_deleted)\n" +
-                    "PARTITION BY joined\n" +
-                    "ORDER BY tuple()\n" +
-                    "SETTINGS index_granularity = 8192"));
+            if(clickHouseServerVersion.contains("latest")) {
+                Assert.assertTrue(membersResult.equalsIgnoreCase("CREATE TABLE employees.members\n" +
+                        "(\n" +
+                        "    `firstname` String,\n" +
+                        "    `lastname` String,\n" +
+                        "    `username` String,\n" +
+                        "    `email` Nullable(String),\n" +
+                        "    `joined` Date32,\n" +
+                        "    `_version` UInt64,\n" +
+                        "    `is_deleted` UInt8\n" +
+                        ")\n" +
+                        "ENGINE = ReplacingMergeTree(_version, is_deleted)\n" +
+                        "PARTITION BY joined\n" +
+                        "ORDER BY tuple()\n" +
+                        "SETTINGS index_granularity = 8192"));
+            } else {
+                Assert.assertTrue(membersResult.equalsIgnoreCase("CREATE TABLE employees.members\n" +
+                        "(\n" +
+                        "    `firstname` String,\n" +
+                        "    `lastname` String,\n" +
+                        "    `username` String,\n" +
+                        "    `email` Nullable(String),\n" +
+                        "    `joined` Date32,\n" +
+                        "    `_sign` Int8,\n" +
+                        "    `_version` UInt64\n" +
+                        ")\n" +
+                        "ENGINE = ReplacingMergeTree(_version)\n" +
+                        "PARTITION BY joined\n" +
+                        "ORDER BY tuple()\n" +
+                        "SETTINGS index_granularity = 8192"));
+            }
 
             String rcxResult = writer.executeQuery("show create table rcx");
 
-            Assert.assertTrue(rcxResult.equalsIgnoreCase("CREATE TABLE employees.rcx\n" +
-                    "(\n" +
-                    "    `a` Int32,\n" +
-                    "    `b` Nullable(Int32),\n" +
-                    "    `c` String,\n" +
-                    "    `d` Int32,\n" +
-                    "    `_version` UInt64,\n" +
-                    "    `is_deleted` UInt8\n" +
-                    ")\n" +
-                    "ENGINE = ReplacingMergeTree(_version, is_deleted)\n" +
-                    "PARTITION BY (a, d, c)\n" +
-                    "ORDER BY tuple()\n" +
-                    "SETTINGS index_granularity = 8192"));
+            if(clickHouseServerVersion.contains("latest")) {
+                Assert.assertTrue(rcxResult.equalsIgnoreCase("CREATE TABLE employees.rcx\n" +
+                        "(\n" +
+                        "    `a` Int32,\n" +
+                        "    `b` Nullable(Int32),\n" +
+                        "    `c` String,\n" +
+                        "    `d` Int32,\n" +
+                        "    `_version` UInt64,\n" +
+                        "    `is_deleted` UInt8\n" +
+                        ")\n" +
+                        "ENGINE = ReplacingMergeTree(_version, is_deleted)\n" +
+                        "PARTITION BY (a, d, c)\n" +
+                        "ORDER BY tuple()\n" +
+                        "SETTINGS index_granularity = 8192"));
+            } else {
+                Assert.assertTrue(rcxResult.equalsIgnoreCase("CREATE TABLE employees.rcx\n" +
+                        "(\n" +
+                        "    `a` Int32,\n" +
+                        "    `b` Nullable(Int32),\n" +
+                        "    `c` String,\n" +
+                        "    `d` Int32,\n" +
+                        "    `_sign` Int8,\n" +
+                        "    `_version` UInt64\n" +
+                        ")\n" +
+                        "ENGINE = ReplacingMergeTree(_version)\n" +
+                        "PARTITION BY (a, d, c)\n" +
+                        "ORDER BY tuple()\n" +
+                        "SETTINGS index_granularity = 8192"));
+            }
 
-            new com.altinity.clickhouse.sink.connector.db.DBMetadata().getTableEngine(writer.getConnection(), "employees", "rmt_test");
+//            new com.altinity.clickhouse.sink.connector.db.DBMetadata().getTableEngine(writer.getConnection(), "employees", "rmt_test");
 
             if(engine.get() != null) {
                 engine.get().stop();
@@ -143,5 +202,64 @@ public class ClickHouseDebeziumEmbeddedDDLTableOperationsIT extends ClickHouseDe
             executorService.shutdown();
 
         }
+
+    Connection connectToMySQL() {
+        Connection conn = null;
+        try {
+
+            String connectionUrl = String.format("jdbc:mysql://%s:%s/%s?user=%s&password=%s", mySqlContainer.getHost(), mySqlContainer.getFirstMappedPort(),
+                    mySqlContainer.getDatabaseName(), mySqlContainer.getUsername(), mySqlContainer.getPassword());
+            conn = DriverManager.getConnection(connectionUrl);
+
+
+        } catch (SQLException ex) {
+            // handle any errors
+
+        }
+
+        return conn;
+    }
+
+    protected Properties getDebeziumProperties() throws Exception {
+
+        // Start the debezium embedded application.
+        Properties defaultProps = (new EnvironmentConfigurationService()).parse();
+        defaultProps.setProperty("database.hostname", mySqlContainer.getHost());
+        defaultProps.setProperty("database.port", String.valueOf(mySqlContainer.getFirstMappedPort()));
+        defaultProps.setProperty("database.user", "root");
+        defaultProps.setProperty("database.password", "adminpass");
+
+        defaultProps.setProperty("database.include.list", "employees");
+        defaultProps.setProperty("snapshot.mode", "initial");
+
+
+        defaultProps.setProperty("snapshot.mode", "initial");
+        defaultProps.setProperty("connector.class", "io.debezium.connector.mysql.MySqlConnector");
+
+        defaultProps.setProperty("include.schema.change", "true");
+        defaultProps.setProperty("include.schema.comments", "true");
+
+        defaultProps.setProperty("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore");
+        defaultProps.setProperty("provide.transaction.metadata", "true");
+        //String tempOffsetPath = "/tmp/2/offsets" + System.currentTimeMillis() + ".dat";
+        Path tmpFilePath = Files.createTempFile("offsets", ".dat");
+        Files.deleteIfExists(tmpFilePath);
+        defaultProps.setProperty("offset.storage.file.filename", tmpFilePath.toAbsolutePath().toString());
+        defaultProps.setProperty("offset.flush.interval.ms", "60000");
+
+        defaultProps.setProperty("auto.create.tables", "true");
+        defaultProps.setProperty("clickhouse.server.url", clickHouseContainer.getHost());
+        defaultProps.setProperty("clickhouse.server.port", String.valueOf(clickHouseContainer.getFirstMappedPort()));
+        defaultProps.setProperty("clickhouse.server.user", "default");
+        defaultProps.setProperty("clickhouse.server.pass", "");
+        defaultProps.setProperty("clickhouse.server.database", "employees");
+        defaultProps.setProperty("replacingmergetree.delete.column", "_sign");
+        defaultProps.setProperty("metrics.port", "8088");
+        defaultProps.setProperty("thread.pool.size", "1");
+        defaultProps.setProperty("database.allowPublicKeyRetrieval", "true");
+        defaultProps.setProperty("metrics.enable", "false");
+
+        return defaultProps;
+    }
 
 }
