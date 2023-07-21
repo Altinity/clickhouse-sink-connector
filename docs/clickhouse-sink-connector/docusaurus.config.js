217450776cd5d0cc32a095d50c265b90a65b676e
Kanthi Subramanian <subkanthi@gmail.com>
2023-07-21 14:42:44 -0400

    Updated docusaurus config page

--- docs/clickhouse-sink-connector/docusaurus.config.js
+++ docs/clickhouse-sink-connector/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'http://docs.altinity.com/clickhouse-sink-connector',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'altinity', // Usually your GitHub org/user name.
+  projectName: 'clickhouse-sink-connector', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
