ccd24a5c75936d46e4a756c415cc7be1395cfcae
kanthi Subramanian <subkanthi@gmail.com>
2023-07-31 15:57:23 -0400

    Updates to docusaurus docs

--- docusaurus/clickhouse-sink-connector/docusaurus.config.js
+++ docusaurus/clickhouse-sink-connector/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Altinity Sink Connector',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -69,7 +69,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Altinity Sink Connector',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
