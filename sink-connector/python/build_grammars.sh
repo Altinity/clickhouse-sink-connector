6d9cb1b1a0219c38eb41942fc95de542ad4c4e2b
Arnaud Adant <aadant@fpif-vcsl1.w2k.jumptrading.com>
2023-10-13 16:20:50 -0500

    Add script to build the grammar

---
+++ sink-connector/python/build_grammars.sh
@@ -0,0 +1,4 @@
+wget https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_linux-x64_bin.tar.gz
+wget https://www.antlr.org/download/antlr-4.11.1-complete.jar
+tar zxvf openjdk-19.0.2_linux-x64_bin.tar.gz
+jdk-19.0.2/bin/java -Xmx500M -cp antlr-4.11.1-complete.jar org.antlr.v4.Tool -Dlanguage=Python3  -no-visitor  antlr_grammars/mysql/MySqlParser.g4  antlr_grammars/mysql/MySqlLexer.g4 -o db_load/mysql_parser
