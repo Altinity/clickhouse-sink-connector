651cadd6bd918f8098d42feecbc32261af527d45
Kanthi Subramanian <subkanthi@gmail.com>
2023-11-03 13:31:54 -0400

    Updated README.md with configuration variables.

--- sink-connector-lightweight/src/test/resources/alter_ddl_add_column.sql
+++ sink-connector-lightweight/src/test/resources/alter_ddl_add_column.sql
@@ -3,3 +3,27 @@ create table add_test(col1 varchar(255), col2 int, col3 int);
 
 insert into ship_class values(1, "test_class", 20.2, 20.2, 1997, 1997, 1998);
 insert into ship_class values(2, "test_class", 20.2, 20.2, 1997, 1997, 1998);
+
+CREATE TABLE `test_ent` (
+  `e_id` int NOT NULL AUTO_INCREMENT,
+  `u_id` int unsigned NOT NULL,
+  `date` date NOT NULL,
+  `t_id` int unsigned NOT NULL,
+  `tspan` int unsigned NOT NULL,
+  `lock_status` smallint DEFAULT '0',
+  `modified_ts` timestamp NULL DEFAULT NULL,
+  `description` varchar(1024) CHARACTER SET utf8mb3 DEFAULT NULL,
+  `start_time_hour` time DEFAULT NULL,
+  `end_time_hour` time DEFAULT NULL,
+  `invoiceId` int DEFAULT '0',
+  `billable` binary(1) DEFAULT '1',
+PRIMARY KEY (`e_id`),
+KEY `user_id` (`u_id`,`date`),
+KEY `test_ent_task_id_date` (`t_id`,`date`),
+KEY `date_u` (`date`,`u_id`),
+KEY `test_ent_entries_task_id_index` (`t_id`),
+KEY `user_t_date` (`u_id`,`t_id`,`date`)
+) ENGINE=InnoDB AUTO_INCREMENT=191099272 DEFAULT CHARSET=latin1;
+
+
+insert into test_ent values(111, 222, '2023-01-01', 222, 222, 1, '2008-01-01 00:00:01', 'Test', '22:10', '23:10', 2, '');
