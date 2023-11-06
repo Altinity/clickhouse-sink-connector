4cf19f91a18f61244fdae22a1044e7fec5f11ea0
Kanthi Subramanian <subkanthi@gmail.com>
2023-11-06 05:13:48 -0500

    Changes to python scripts

--- sink-connector/tests/diff_datatypes_lightweight_data.sh
+++ sink-connector/tests/diff_datatypes_lightweight_data.sh
@@ -3,7 +3,7 @@ cd ../python
 DATABASE=sbtest
 
 python3 db_compare/mysql_table_checksum.py --mysql_host localhost --mysql_user root --mysql_password root  --mysql_database $DATABASE --tables_regex "^*" --debug_output
-python3 db_compare/clickhouse_table_checksum.py --clickhouse_host localhost --clickhouse_user root --clickhouse_password root  --clickhouse_database test --tables_regex "^*" --debug_output --sign_column=is_deleted --new_rmt=True --exclude-columns=['_version', 'is_deleted']
+python3 db_compare/clickhouse_table_checksum.py --clickhouse_host localhost --clickhouse_user root --clickhouse_password root  --clickhouse_database test --tables_regex "^*" --debug_output --sign_column=is_deleted --new_rmt=True --exclude_columns=_sign,_version,is_deleted
 
 mysql_information_schema_query="select TABLE_NAME from information_schema.tables where TABLE_SCHEMA='$DATABASE'"
 echo $mysql_information_schema_query
@@ -20,11 +20,11 @@ do
   ch_file_name="$(pwd)/out.${table_name}.ch.txt"
 
 
-  echo $mysql_file_name
-  echo $ch_file_name
+#  echo $mysql_file_name
+#  echo $ch_file_name
   if test -f ${mysql_file_name}; then
     if test -f ${ch_file_name}; then
-        echo " DIFF TABLE ****${table_name} **** "
+        #echo " DIFF TABLE ****${table_name} **** "
         diff $(pwd)/out.${table_name}.mysql.txt $(pwd)/out.${table_name}.ch.txt
         if [[ $? != 0 ]]; then
           echo "string types diff failed"
