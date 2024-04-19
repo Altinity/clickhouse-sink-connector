ceb2ec0b874655471461909d1b4f1e17af1ce6bb
Kanthi Subramanian <subkanthi@gmail.com>
2024-04-19 16:56:11 -0400

    [528] Added logic to create view for replica_source_info table

--- sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/cdc/ClickHouseDelayedStartIT.java
+++ sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/cdc/ClickHouseDelayedStartIT.java
@@ -124,4 +124,47 @@ public class ClickHouseDelayedStartIT {
         clickHouseDebeziumEmbeddedApplication.getDebeziumEventCapture().stop();
     }
 
+    @Test
+    @DisplayName("Validates that the debezium storage view is created successfully")
+    public void debeziumStorageView() throws Exception {
+
+        Injector injector = Guice.createInjector(new AppInjector());
+
+        Properties props = getDebeziumPropertiesForSchemaOnly(mySqlContainer, clickHouseContainer);
+        ClickHouseDebeziumEmbeddedApplication clickHouseDebeziumEmbeddedApplication = new ClickHouseDebeziumEmbeddedApplication();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.execute(() -> {
+            try {
+                clickHouseDebeziumEmbeddedApplication.start(injector.getInstance(DebeziumRecordParserService.class),
+                        injector.getInstance(DDLParserService.class), props, false);
+                DebeziumEmbeddedRestApi.startRestApi(props, injector, clickHouseDebeziumEmbeddedApplication.getDebeziumEventCapture()
+                        , new Properties());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread.sleep(10000);
+
+        // Connect to clickhouse and validate that the view was created successfully.
+        String jdbcUrl = BaseDbWriter.getConnectionString(clickHouseContainer.getHost(), clickHouseContainer.getFirstMappedPort(),
+                "altinity_sink_connector");
+        ClickHouseConnection chConn = BaseDbWriter.createConnection(jdbcUrl, "Client_1",
+                clickHouseContainer.getUsername(), clickHouseContainer.getPassword(), new ClickHouseSinkConnectorConfig(new HashMap<>()));
+        BaseDbWriter writer = new BaseDbWriter(clickHouseContainer.getHost(), clickHouseContainer.getFirstMappedPort(),
+                "altinity_sink_connector", clickHouseContainer.getUsername(), clickHouseContainer.getPassword(), null, chConn);
+
+
+        // Check if the view altinity_sink_connector.show_replica_status was created successfully.
+        ResultSet resultSet = writer.executeQueryWithResultSet("show create view altinity_sink_connector.show_replica_status");
+
+        boolean viewCheck = false;
+        while (resultSet.next()) {
+            viewCheck = true;
+            Assert.assertTrue(resultSet.getString("statement").contains("CREATE VIEW altinity_sink_connector.show_replica_status"));
+            break;
+        }
+        Assert.assertTrue(viewCheck);
+    }
 }
