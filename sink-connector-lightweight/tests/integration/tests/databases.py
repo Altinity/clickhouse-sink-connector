360588ceb46eba98bf8c3a4d6641afdddbb675d9
selfeer <selfeers@gmail.com>
2024-06-10 21:32:02 +0400

    fix

--- sink-connector-lightweight/tests/integration/tests/databases.py
+++ sink-connector-lightweight/tests/integration/tests/databases.py
@@ -22,26 +22,23 @@ def databases_tables(
         create_mysql_table(
             table_name=table_name,
             columns=mysql_columns,
+            database_name=database_1,
         )
 
     with And(
         f"I create another database in Clickhouse and table with the same name {table_name} in it"
     ):
-        clickhouse_node = self.context.cluster.node("clickhouse")
 
-        create_clickhouse_database(name="test2")
+        create_clickhouse_database(name=database_2)
+        create_mysql_database(database_name=database_2)
 
-        clickhouse_node.query(
-            f"CREATE TABLE IF NOT EXISTS {database_2}.{table_name} "
-            f"(id Int32, MyData Nullable(DateTime64(3)), _sign "
-            f"Int8, {version_column} UInt64) "
-            f"ENGINE = ReplacingMergeTree({version_column}) "
-            f" PRIMARY KEY (id ) ORDER BY (id)"
-            f" SETTINGS "
-            f"index_granularity = 8192;"
+        create_mysql_table(
+            table_name=table_name,
+            columns=mysql_columns,
+            database_name=database_2,
         )
 
-    with When(f"I insert data in MySQL table {table_name}"):
+    with When(f"I insert data in MySQL table {table_name} on {database_1}"):
         mysql.query(
             f"INSERT INTO {database_1}.{table_name} VALUES (1, '2018-09-08 17:51:05.777')"
         )
@@ -52,11 +49,25 @@ def databases_tables(
             f"INSERT INTO {database_1}.{table_name} VALUES (3, '2018-09-08 17:51:05.777')"
         )
 
-    with Then(f"I check that data is replicated to the correct table"):
-        check_if_table_was_created(
-            table_name=table_name,
-            database_name="test",
+    with And(f"I insert data in MySQL table {table_name} on {database_2}"):
+        mysql.query(
+            f"INSERT INTO {database_2}.{table_name} VALUES (1, '2018-09-08 17:51:05.777')"
         )
+        mysql.query(
+            f"INSERT INTO {database_2}.{table_name} VALUES (2, '2018-09-08 17:51:05.777')"
+        )
+        mysql.query(
+            f"INSERT INTO {database_2}.{table_name} VALUES (3, '2018-09-08 17:51:05.777')"
+        )
+
+    with Then(f"I check that data is replicated to the correct table"):
+        with Pool(2) as executor:
+            Check(test=check_if_table_was_created, parallel=True, executor=executor)(
+                table_name=table_name, database_name=database_1
+            )
+            Check(test=check_if_table_was_created, parallel=True, executor=executor)(
+                table_name=table_name, database_name=database_2
+            )
 
 
 @TestModule
