391c35f9e726e98e7003310c716dc525653d37d3
Kanthi Subramanian <subkanthi@gmail.com>
2024-06-11 13:32:10 -0400

    Use system database to create offsets/schema history storage

--- sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/cdc/DebeziumChangeEventCapture.java
+++ sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/cdc/DebeziumChangeEventCapture.java
@@ -300,7 +300,7 @@ public class DebeziumChangeEventCapture {
             DBCredentials dbCredentials = parseDBConfiguration(config);
             //if (writer == null) {
                 String jdbcUrl = BaseDbWriter.getConnectionString(dbCredentials.getHostName(), dbCredentials.getPort(),
-                        dbCredentials.getDatabase());
+                        "system");
                 ClickHouseConnection conn = BaseDbWriter.createConnection(jdbcUrl, "Client_1",dbCredentials.getUserName(), dbCredentials.getPassword(), config);
                 BaseDbWriter writer = new BaseDbWriter(dbCredentials.getHostName(), dbCredentials.getPort(),
                         dbCredentials.getDatabase(), dbCredentials.getUserName(),
