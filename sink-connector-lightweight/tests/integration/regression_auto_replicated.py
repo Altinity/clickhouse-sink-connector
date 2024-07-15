98312c09b07bc77e19f65d0678afa71f4bdcad2f
selfeer <selfeers@gmail.com>
2024-07-15 18:14:29 +0400

    fix xfail

--- sink-connector-lightweight/tests/integration/regression_auto_replicated.py
+++ sink-connector-lightweight/tests/integration/regression_auto_replicated.py
@@ -133,7 +133,7 @@ ffails = {
         Skip,
         "Seems to be broken in CI/CD. need to fix.",
     ),
-    "/mysql to clickhouse replication/auto table creation/parallel alters/multiple parallel add modify drop column": (
+    "/mysql to clickhouse replication/auto replicated table creation/parallel alters/multiple parallel add modify drop column": (
         Skip,
         "Test requires fixing.",
     ),
