ea42f39eef3f4e394e0a4e53df907eae62c6ab54
Kanthi Subramanian <subkanthi@gmail.com>
2024-10-01 16:16:13 -0400

    Added logic to not add NULLABLE for Point data types

--- sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/CreateTableDataTypesIT.java
+++ sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/CreateTableDataTypesIT.java
@@ -267,28 +267,30 @@ public class CreateTableDataTypesIT extends DDLBaseIT {
         // validate POINT data type
         // Create a new table with POINT data type
         // Crate a new table on MySQL with POINT data type
-        String createTableWithPoint = "CREATE TABLE employees.point_table (id int, c1 int, c2 int, c3a point, c3b point, f1 float(10), f2 decimal(8,4) primary key(id))";
+        String createTableWithPoint = "CREATE TABLE employees.point_table (id int not null PRIMARY KEY, c1 int, c2 int, c3a POINT, c3b POINT, f1 float(10), f2 decimal(8,4))";
         ITCommon.connectToMySQL(mySqlContainer).createStatement().execute(createTableWithPoint);
 
         // Sleep for 10 seconds to allow the table to be replicated
         Thread.sleep(10000);
 
         // Insert a new row into the table
-        ITCommon.connectToMySQL(mySqlContainer).createStatement().execute("INSERT INTO employees.point_table (id, c1, c2, c3a, c3b, f1, f2) values (1, 123, 456, '(1,2)', '(3,4)', 100.20, 100.20)");
+        ITCommon.connectToMySQL(mySqlContainer).createStatement().execute("INSERT INTO employees.point_table (id, c1, c2, c3a, c3b, f1, f2) values (1, 123, 456, POINT(1.0,2.0), POINT(3.0,4.0), 100.20, 100.20)");
 
         Thread.sleep(10000);
         ResultSet rs = writer.executeQueryWithResultSet("select * from employees.point_table");
         boolean pointResultValidated = false;
         while(rs.next()) {
             pointResultValidated = true;
-            Assert.assertTrue(rs.getString("c3a").equalsIgnoreCase("(1,2)"));
-            Assert.assertTrue(rs.getString("c3b").equalsIgnoreCase("(3,4)"));
+            String c3a = rs.getString("c3a");
+            String c3b = rs.getString("c3b");
+            Assert.assertTrue(c3a.equalsIgnoreCase("(1.0,2.0)"));
+            Assert.assertTrue(c3b.equalsIgnoreCase("(3.0,4.0)"));
         }
         Assert.assertTrue(pointResultValidated);
 
 
 
-
+        Thread.sleep(5000);
 
         if(engine.get() != null) {
             engine.get().stop();
