b0e80c03f6ce65ffac6d250249fbcb930df5bac2
Kanthi Subramanian <subkanthi@gmail.com>
2024-10-11 17:25:44 -0400

    Added fix to retrieve data type for columns with comments

--- sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/MySqlDDLParserListenerImpl.java
+++ sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/MySqlDDLParserListenerImpl.java
@@ -362,9 +362,10 @@ public class MySqlDDLParserListenerImpl extends MySQLDDLParserBaseListener {
             // Dont try to get precision/scale for enums
         }
         else if(parsedDataType.contains("(") && parsedDataType.contains(")") && parsedDataType.contains(",") ) {
+            String sanitizedDataType = parsedDataType.split("COMMENT")[0].trim();
             try {
-                precision = Integer.parseInt(parsedDataType.substring(parsedDataType.indexOf("(") + 1, parsedDataType.indexOf(",")));
-                scale = Integer.parseInt(parsedDataType.substring(parsedDataType.indexOf(",") + 1, parsedDataType.indexOf(")")));
+                precision = Integer.parseInt(sanitizedDataType.substring(sanitizedDataType.indexOf("(") + 1, sanitizedDataType.indexOf(",")));
+                scale = Integer.parseInt(sanitizedDataType.substring(sanitizedDataType.indexOf(",") + 1, sanitizedDataType.indexOf(")")));
             } catch(Exception e) {
                 log.error("Error parsing precision, scale : columnName" + columnName);
             }
