3f2b7c4075c101796d087e930aae9268c00a86c4
Kanthi Subramanian <subkanthi@gmail.com>
2025-02-05 15:00:31 -0500

    Set connection pool name and updated grafana dashboard.

--- sink-connector/src/main/java/com/altinity/clickhouse/sink/connector/db/HikariDbSource.java
+++ sink-connector/src/main/java/com/altinity/clickhouse/sink/connector/db/HikariDbSource.java
@@ -43,6 +43,7 @@ public class HikariDbSource {
 
      
         HikariConfig poolConfig = new HikariConfig();
+        poolConfig.setPoolName("clickhouse" + "-" + databaseName);
         String jdbcUrl = String.format("jdbc:ch:{hostname}:{port}/%s?insert_quorum=auto&server_time_zone&server_version=22.13.1.24495", databaseName);
         poolConfig.setJdbcUrl(jdbcUrl);
         poolConfig.setDriverClassName("com.clickhouse.jdbc.ClickHouseDriver"); // Ensure driver is set
