b9783a6c44983a19314a8533b597c314fa8c1c26
Kanthi Subramanian <subkanthi@gmail.com>
2025-02-06 16:53:54 -0500

    fixed integration tests.

--- sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ClickHouseDebeziumEmbeddedPostgresDecoderBufsDockerIT.java
+++ sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ClickHouseDebeziumEmbeddedPostgresDecoderBufsDockerIT.java
@@ -97,7 +97,7 @@ public class ClickHouseDebeziumEmbeddedPostgresDecoderBufsDockerIT {
 
 
         int tmCount = 0;
-        ResultSet chRs = writer.getConnection().prepareStatement("select count(*) from tm").executeQuery();
+        ResultSet chRs = writer.getConnection().prepareStatement("select count(*) from public.tm").executeQuery();
         while(chRs.next()) {
             tmCount =  chRs.getInt(1);
         }
