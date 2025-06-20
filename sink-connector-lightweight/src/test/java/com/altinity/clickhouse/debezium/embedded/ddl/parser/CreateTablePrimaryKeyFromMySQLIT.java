60be2af2afaa6917de67edaad0067445085ff320
Kanthi Subramanian <subkanthi@gmail.com>
2025-06-20 14:29:46 -0400

    NULL Primary Key from MySQL needs to be translated to CH.

---
+++ sink-connector-lightweight/src/test/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/CreateTablePrimaryKeyFromMySQLIT.java
@@ -0,0 +1,131 @@
+package com.altinity.clickhouse.debezium.embedded.ddl.parser;
+
+import com.altinity.clickhouse.debezium.embedded.ITCommon;
+import com.altinity.clickhouse.debezium.embedded.cdc.DebeziumChangeEventCapture;
+import com.altinity.clickhouse.debezium.embedded.parser.SourceRecordParserService;
+import com.altinity.clickhouse.sink.connector.db.BaseDbWriter;
+import com.altinity.clickhouse.sink.connector.db.DBMetadata;
+import com.altinity.clickhouse.sink.connector.db.HikariDbSource;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.clickhouse.ClickHouseContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
+
+import java.sql.Connection;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.altinity.clickhouse.debezium.embedded.ITCommon.connectToMySQL;
+import static com.altinity.clickhouse.debezium.embedded.ITCommon.getDebeziumProperties;
+
+@Testcontainers
+@DisplayName("Integration test to verify Primary Key from MySQL should be translated to non-null type in ClickHouse")
+public class CreateTablePrimaryKeyFromMySQLIT {
+
+    protected MySQLContainer mySqlContainer;
+
+    @Container
+    public static ClickHouseContainer clickHouseContainer = new ClickHouseContainer(DockerImageName.parse("clickhouse/clickhouse-server:latest")
+            .asCompatibleSubstituteFor("clickhouse"))
+            .withInitScript("init_clickhouse_it.sql")
+            .withCopyFileToContainer(MountableFile.forClasspathResource("config.xml"), "/etc/clickhouse-server/config.d/config.xml")
+            .withUsername("ch_user")
+            .withPassword("password")
+            .withExposedPorts(8123);
+
+    @BeforeEach
+    public void startContainers() throws InterruptedException {
+        mySqlContainer = new MySQLContainer<>(DockerImageName.parse("docker.io/bitnami/mysql:8.0.36")
+                .asCompatibleSubstituteFor("mysql"))
+                .withDatabaseName("employees").withUsername("root").withPassword("adminpass")
+                //  .withInitScript("data_types.sql")
+                .withExtraHost("mysql-server", "0.0.0.0")
+                .waitingFor(new HttpWaitStrategy().forPort(3306));
+
+        BasicConfigurator.configure();
+        mySqlContainer.start();
+        clickHouseContainer.start();
+        Thread.sleep(25000);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        mySqlContainer.stop();
+        clickHouseContainer.stop();
+    }
+
+    /**
+     * Primary Key from MySQL should be translated to non-null type in ClickHouse
+     * @throws Exception
+     */
+    @Test
+    public void testPrimaryKeyFromMySQL() throws Exception {
+        AtomicReference<DebeziumChangeEventCapture> engine = new AtomicReference<>();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.execute(() -> {
+            try {
+
+                Properties props = getDebeziumProperties(mySqlContainer, clickHouseContainer);
+                engine.set(new DebeziumChangeEventCapture());
+                engine.get().setup(props, new SourceRecordParserService(),  false);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread.sleep(30000);
+
+        Connection conn = connectToMySQL(mySqlContainer);
+
+        conn.prepareStatement("\n" +
+                "CREATE TABLE employees.contacts (id INT PRIMARY KEY,\n" +
+                "first_name VARCHAR(50) NOT NULL,\n" +
+                "last_name VARCHAR(50) NOT NULL,\n" +
+                "fullname varchar(101) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)),\n" +
+                "email VARCHAR(100) NOT NULL,\n" +
+                "gmt_time DATETIME NOT NULL);\n").execute();
+
+        Thread.sleep(30000);
+
+        conn.prepareStatement("insert into contacts(id,first_name, last_name, email , gmt_time) values(1,'John', 'Doe', 'john.doe@gmail.com','2025-04-10 12:34:56')").execute();
+        Thread.sleep(20000);
+
+        BaseDbWriter writer = ITCommon.getDBWriter(clickHouseContainer);
+        DBMetadata dbMetadata = new DBMetadata();
+        Map<String, String> columnsToDataTypeMap = dbMetadata.getColumnsDataTypesForTable(writer.getConnection(), "contacts", "employees");
+
+        Assert.assertTrue(columnsToDataTypeMap.get("id").equalsIgnoreCase("Int32"));
+        Assert.assertTrue(columnsToDataTypeMap.get("first_name").equalsIgnoreCase("String"));
+        Assert.assertTrue(columnsToDataTypeMap.get("last_name").equalsIgnoreCase("String"));
+        Assert.assertTrue(columnsToDataTypeMap.get("fullname").equalsIgnoreCase("Nullable(String)"));
+        Assert.assertTrue(columnsToDataTypeMap.get("email").equalsIgnoreCase("String"));
+        // Assert.assertTrue(columnsToDataTypeMap.get("gmt_time").equalsIgnoreCase("String"));
+
+        Thread.sleep(10000);
+
+        writer.getConnection().close();
+
+        Thread.sleep(10000);
+
+        if(engine.get() != null) {
+            engine.get().stop();
+        }
+        // Files.deleteIfExists(tmpFilePath);
+        executorService.shutdown();
+
+        HikariDbSource.close();
+    }
+}
