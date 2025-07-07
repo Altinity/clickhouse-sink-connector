d0d9a754e49e7d93553979db2f689c6f8591ce3d
Kanthi Subramanian <subkanthi@gmail.com>
2025-07-07 11:37:59 -0500

    [1097] Support longtext MySQL DDL.

--- sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/MySqlDDLParserListenerImpl.java
+++ sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/ddl/parser/MySqlDDLParserListenerImpl.java
@@ -282,7 +282,23 @@ public class MySqlDDLParserListenerImpl extends MySQLDDLParserBaseListener {
         if (orderByColumns.length() == 0) {
             this.query.append(Constants.ORDER_BY_TUPLE);
         } else {
-            this.query.append(Constants.ORDER_BY).append(orderByColumns.toString());
+
+            // Convert the orderByColumns object to a string
+            String orderByStr = orderByColumns.toString();
+
+            // Regex pattern to detect invalid column suffix like id_registro(10)
+            String regex = "\\b(\\w+)\\(\\d+\\)";
+
+            if (orderByStr.matches(".*" + regex + ".*")) {
+                // If pattern is matched: clean up suffix and append ORDER BY
+                String fixedOrderBy = orderByStr.replaceAll(regex, "$1");
+
+                // Append the sanitized ORDER BY clause to the query
+                this.query.append(Constants.ORDER_BY).append(fixedOrderBy);
+            } else {
+                // If no prefix match found, use ReplacingMergeTree engine clause instead
+                this.query.append(Constants.ORDER_BY).append(orderByStr);
+            }
         }
     }
 
