65a59cb92cb45945faff218e4bd4cabbe5980ae5
congye <cong.ye@finnplay.com>
2026-07-07 15:35:15 +0800

    filter the env to avoid injecting all envs

--- sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/ClickHouseDebeziumEmbeddedApplication.java
+++ sink-connector-lightweight/src/main/java/com/altinity/clickhouse/debezium/embedded/ClickHouseDebeziumEmbeddedApplication.java
@@ -13,6 +13,9 @@ import com.altinity.clickhouse.sink.connector.ClickHouseSinkConnectorConfigVaria
 import com.altinity.clickhouse.sink.connector.db.BaseDbWriter;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -234,7 +237,12 @@ public class ClickHouseDebeziumEmbeddedApplication {
     }
 
     private static void loadEnvs() {
-        props.putAll(System.getenv());
+        Map<String, String> propsFromEnvs = System.getenv().entrySet().stream()
+                                                  .filter(entry -> entry.getKey().startsWith("lightweight."))
+                                                  .collect(Collectors.toMap(entry -> entry.getKey().replaceFirst("lightweight.", ""),
+                                                                            Entry::getValue));
+
+        props.putAll(propsFromEnvs);
     }
 
     /**
